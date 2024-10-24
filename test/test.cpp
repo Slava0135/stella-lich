914e5964dfb99a3bd44e47c163563badbd3dac71
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-24 18:30:20 +0300

    test: remove unused tag

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <mark_and_sweep.hpp>
 
-TEST_CASE("no objects", "[what]") {
+TEST_CASE("no objects") {
   auto collector = gc::MarkAndSweep(0);
   REQUIRE(collector.get_stats().alive == 0);
 }
