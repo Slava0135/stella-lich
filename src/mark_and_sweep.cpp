3783f2e5d90811c4928d6d63023f5c6932959612
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-25 10:45:25 +0300

    test: sanitize address

--- src/mark_and_sweep.cpp
+++ src/mark_and_sweep.cpp
@@ -1,7 +1,5 @@
 #include "mark_and_sweep.hpp"
 
 namespace gc {
-MarkAndSweep::Stats const& MarkAndSweep::get_stats() {
-    return this->stats_;
-}
-}
+MarkAndSweep::Stats const& MarkAndSweep::get_stats() { return this->stats_; }
+}  // namespace gc
--- src/mark_and_sweep.hpp
+++ src/mark_and_sweep.hpp
@@ -2,6 +2,8 @@
 
 #include <stddef.h>
 
+#include <memory>
+
 namespace gc {
 
 class MarkAndSweep {
@@ -12,12 +14,19 @@ class MarkAndSweep {
   const size_t max_memory;
 
   MarkAndSweep(const size_t max_memory)
-      : max_memory(max_memory), stats_(Stats{.alive = 0}) {}
+      : max_memory(max_memory), stats_(Stats{.alive = 0}) {
+    space_ = new void*[max_memory];
+  }
 
   Stats const& get_stats();
 
+  ~MarkAndSweep() {
+    delete[] space_;
+  }
+
  private:
   Stats stats_;
+  void** space_;
 };
 
 }  // namespace gc
