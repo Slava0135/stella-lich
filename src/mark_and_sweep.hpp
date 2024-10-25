6ffd3b9f708b22f857079711b1a8028502c53c20
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-25 11:20:55 +0300

    use `unsigned char[]` instead of `void**`

--- src/mark_and_sweep.hpp
+++ src/mark_and_sweep.hpp
@@ -17,7 +17,7 @@ class MarkAndSweep {
 
   MarkAndSweep(const size_t max_memory)
       : max_memory(max_memory), stats_(Stats{.n_alive = 0, .n_roots = 0}) {
-    space_ = new void*[max_memory];
+    space_ = std::make_unique<unsigned char[]>(max_memory);
   }
 
   Stats const& get_stats() const;
@@ -25,12 +25,10 @@ class MarkAndSweep {
   void push_root(void** root);
   void pop_root(void** root);
 
-  ~MarkAndSweep() { delete[] space_; }
-
  private:
   Stats stats_;
-  void** space_;
-  std::vector<void **> roots_;
+  std::unique_ptr<unsigned char[]> space_;
+  std::vector<void**> roots_;
 };
 
 }  // namespace gc
