2d7356f2676fb7526b86182c0020b7e01f032b64
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-26 12:06:47 +0300

    block size

--- src/mark_and_sweep.cpp
+++ src/mark_and_sweep.cpp
@@ -3,14 +3,14 @@
 #include <assert.h>
 
 namespace gc {
-MarkAndSweep::Stats const& MarkAndSweep::get_stats() const {
+MarkAndSweep::Stats const &MarkAndSweep::get_stats() const {
   return this->stats_;
 }
-void MarkAndSweep::push_root(void** root) {
+void MarkAndSweep::push_root(void **root) {
   this->stats_.n_roots++;
   this->roots_.push_back(root);
 }
-void MarkAndSweep::pop_root(void** root) {
+void MarkAndSweep::pop_root(void **root) {
   assert(this->stats_.n_roots > 0 &&
          "roots must not be empty when poping root");
   this->stats_.n_roots--;
@@ -18,40 +18,35 @@ void MarkAndSweep::pop_root(void** root) {
          "the root must be at the top of the stack");
   this->roots_.pop_back();
 }
-void* MarkAndSweep::allocate(std::size_t bytes) {
+void *MarkAndSweep::allocate(std::size_t bytes) {
   assert(bytes > 0 && "can't allocate 0 bytes");
-  auto to_allocate = bytes;
-  bool has_pointers = true;
-  // if object size is less than pointer size then it can't contain pointers and
-  // we don't need `done` array
-  if (to_allocate < this->pointer_size) {
-    to_allocate = this->pointer_size;
-    has_pointers = false;
-  } else {
-    to_allocate += this->done_size;
-    auto offset = to_allocate % this->pointer_size;
-    if (offset) {
-      to_allocate += (pointer_size - offset);
-    }
+  // BLOCK STRUCTURE
+  // everything aligned to pointer size
+  // 
+  //                    -1 | block_size | done array    
+  // object pointer -->  0 | field
+  //                     1 | ...
+  auto to_allocate = this->block_size_size + this->done_size + bytes;
+  auto offset = to_allocate % this->pointer_size;
+  if (offset) {
+    to_allocate += (pointer_size - offset);
   }
   assert(to_allocate % pointer_size == 0 &&
          "object address must be aligned to pointer size");
-  if (has_pointers) {
-    assert(bytes + done_size <= to_allocate &&
-           "memory at all times must fit all object fields and `done` array");
-    assert(to_allocate >= this->pointer_size + this->done_size &&
-           "not enough memory for algorithm");
-  }
+  assert(this->block_size_size + this->done_size + bytes <= to_allocate &&
+         "memory at all times must fit all object fields and meta info");
+  assert(to_allocate >= this->pointer_size + this->done_size &&
+         "not enough memory for algorithm");
   size_t next_bytes_allocated = this->stats_.bytes_allocated + to_allocate;
   if (next_bytes_allocated <= this->max_memory) {
     this->stats_.n_alive++;
     this->stats_.bytes_allocated = next_bytes_allocated;
-    return reinterpret_cast<void*>(next_bytes_allocated);
+    return reinterpret_cast<void *>(next_bytes_allocated);
   } else {
     return nullptr;
   }
 }
-const std::vector<void**>& MarkAndSweep::get_roots() const {
+const std::vector<void **> &MarkAndSweep::get_roots() const {
   return this->roots_;
 }
-}  // namespace gc
+} // namespace gc
--- src/mark_and_sweep.hpp
+++ src/mark_and_sweep.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <stddef.h>
-
+#include <assert.h>
 #include <memory>
+#include <stddef.h>
 #include <vector>
 
 namespace gc {
 
 class MarkAndSweep {
- public:
+public:
   struct Stats {
     size_t n_alive;
     size_t n_roots;
@@ -16,28 +16,30 @@ class MarkAndSweep {
   };
   const size_t max_memory;
   const size_t pointer_size;
-  const size_t done_size;
 
-  MarkAndSweep(const size_t max_memory, const size_t pointer_size, const size_t done_size)
-      : max_memory(max_memory),
-        pointer_size(pointer_size),
-        done_size(done_size),
+  MarkAndSweep(const size_t max_memory, const size_t pointer_size)
+      : max_memory(max_memory), pointer_size(pointer_size),
+        done_size(pointer_size / 2), block_size_size(pointer_size / 2),
         stats_(Stats{.n_alive = 0, .n_roots = 0, .bytes_allocated = 0}) {
+    assert((pointer_size & (pointer_size - 1)) == 0 &&
+           "pointer size must be power of 2");
     space_ = std::make_unique<unsigned char[]>(max_memory);
   }
 
-  Stats const& get_stats() const;
-  const std::vector<void**>& get_roots() const;
+  Stats const &get_stats() const;
+  const std::vector<void **> &get_roots() const;
 
-  void push_root(void** root);
-  void pop_root(void** root);
+  void push_root(void **root);
+  void pop_root(void **root);
 
-  void* allocate(std::size_t bytes);
+  void *allocate(std::size_t bytes);
 
- private:
+private:
+  const size_t done_size;
+  const size_t block_size_size;
   Stats stats_;
   std::unique_ptr<unsigned char[]> space_;
-  std::vector<void**> roots_;
+  std::vector<void **> roots_;
 };
 
-}  // namespace gc
+} // namespace gc
