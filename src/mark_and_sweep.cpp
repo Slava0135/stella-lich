7cae83d2def2e735506d40106f1f7fe7f2efcf76
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-26 12:54:33 +0300

    freelist init (wip)

--- src/mark_and_sweep.cpp
+++ src/mark_and_sweep.cpp
@@ -22,11 +22,11 @@ void *MarkAndSweep::allocate(std::size_t bytes) {
   assert(bytes > 0 && "can't allocate 0 bytes");
   // BLOCK STRUCTURE
   // everything aligned to pointer size
-  // 
-  //                    -1 | block_size | done array    
-  // object pointer -->  0 | field
-  //                     1 | ...
-  auto to_allocate = this->block_size_size_ + this->done_size_ + bytes;
+  //
+  // meta information --> -1 | block_size | done array
+  // object pointer   -->  0 | field
+  //                       1 | ...
+  auto to_allocate = meta_size_ + bytes;
   auto offset = to_allocate % this->pointer_size;
   if (offset) {
     to_allocate += (pointer_size - offset);
@@ -46,5 +46,28 @@ void *MarkAndSweep::allocate(std::size_t bytes) {
 }
 const std::vector<void **> &MarkAndSweep::get_roots() const {
   return this->roots_;
+}
+void MarkAndSweep::set_block_size(size_t obj_idx, size_t size) {
+  assert(size < (static_cast<size_t>(1 << (block_size_size_))) &&
+         "block size can't exceed limit");
+  assert(block_size_size_ + done_size_ <= obj_idx);
+  assert(block_size_size_ + done_size_ <= max_memory);
+  size_t block_size_idx = obj_idx - done_size_ - block_size_size_;
+  void *block_size_ref = &space_[block_size_idx];
+  switch (block_size_size_) {
+  case 0:
+    break;
+  case 1:
+    *reinterpret_cast<uint8_t *>(block_size_ref) = size;
+    break;
+  case 2:
+    *reinterpret_cast<uint16_t *>(block_size_ref) = size;
+    break;
+  case 4:
+    *reinterpret_cast<uint16_t *>(block_size_ref) = size;
+    break;
+  default:
+    assert("unsupported block size");
+  }
 }
 } // namespace gc
--- src/mark_and_sweep.hpp
+++ src/mark_and_sweep.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <assert.h>
+#include <cstdint>
 #include <memory>
 #include <stddef.h>
 #include <vector>
@@ -20,12 +21,15 @@ public:
   MarkAndSweep(const size_t max_memory, const size_t pointer_size)
       : max_memory(max_memory), pointer_size(pointer_size),
         done_size_(pointer_size / 2), block_size_size_(pointer_size / 2),
+        meta_size_(pointer_size),
         stats_(Stats{.n_alive = 0, .n_roots = 0, .bytes_allocated = 0}) {
     assert((pointer_size & (pointer_size - 1)) == 0 &&
            "pointer size must be power of 2");
     space_ = std::make_unique<unsigned char[]>(max_memory);
     space_start_ = space_.get();
     space_end_ = &space_[max_memory - 1];
+    set_block_size(meta_size_ + 1, max_memory - meta_size_);
+    freelist_ = &space_[meta_size_ + 1];
   }
 
   Stats const &get_stats() const;
@@ -39,16 +43,21 @@ public:
 private:
   const size_t done_size_;
   const size_t block_size_size_;
+  const size_t meta_size_;
+
   const void *space_start_;
   const void *space_end_;
 
   Stats stats_;
   std::unique_ptr<unsigned char[]> space_;
   std::vector<void **> roots_;
+  void *freelist_;
 
-  bool is_in_space(void *address) {
-    return space_start_ <= address && address <= space_end_;
+  bool is_in_space(void *obj) {
+    return space_start_ <= obj && obj <= space_end_;
   }
+
+  void set_block_size(size_t obj_idx, size_t size);
 };
 
 } // namespace gc
