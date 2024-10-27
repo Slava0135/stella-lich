ac9d3ff7bf60645961b2744762c655a70e5a1c46
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-27 12:28:49 +0300

    use testcases instead of sections

--- test/test.cpp
+++ test/test.cpp
@@ -55,93 +55,93 @@ TEST_CASE("allocate") {
   REQUIRE(collector.get_stats().bytes_free == 16);
 }
 
-TEST_CASE("collect") {
-  SECTION("no alive objects") {
-    gc::MarkAndSweep collector(1000);
-    REQUIRE(collector.allocate(8) != nullptr);
-    REQUIRE(collector.get_stats().n_alive == 1);
-    REQUIRE(collector.get_stats().n_blocks == 2);
-    REQUIRE(collector.get_stats().bytes_allocated == 16);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-    REQUIRE(collector.allocate(16) != nullptr);
-    REQUIRE(collector.get_stats().n_alive == 2);
-    REQUIRE(collector.get_stats().n_blocks == 3);
-    REQUIRE(collector.get_stats().bytes_allocated == 16 + 24);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-    REQUIRE(collector.allocate(24) != nullptr);
-    REQUIRE(collector.get_stats().n_alive == 3);
-    REQUIRE(collector.get_stats().n_blocks == 4);
-    REQUIRE(collector.get_stats().bytes_allocated == 16 + 24 + 32);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-    collector.collect();
-    REQUIRE(collector.get_stats().n_alive == 0);
-    REQUIRE(collector.get_stats().n_blocks == 4);
-    REQUIRE(collector.get_stats().bytes_allocated == 0);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-  }
-  SECTION("one alive object") {
-    gc::MarkAndSweep collector(1000);
-    void *obj = collector.allocate(8);
-    REQUIRE(obj != nullptr);
-    REQUIRE(collector.get_stats().n_alive == 1);
-    REQUIRE(collector.get_stats().n_blocks == 2);
-    REQUIRE(collector.get_stats().bytes_allocated == 16);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-    collector.push_root(&obj);
-    collector.collect();
-    REQUIRE(collector.get_stats().n_alive == 1);
-    REQUIRE(collector.get_stats().n_blocks == 2);
-    REQUIRE(collector.get_stats().bytes_allocated == 16);
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-  }
-  SECTION("example 13.4 (A. Appel)") {
-    gc::MarkAndSweep collector(1000);
+TEST_CASE("collect - no alive objects") {
+  gc::MarkAndSweep collector(1000);
+  REQUIRE(collector.allocate(8) != nullptr);
+  REQUIRE(collector.get_stats().n_alive == 1);
+  REQUIRE(collector.get_stats().n_blocks == 2);
+  REQUIRE(collector.get_stats().bytes_allocated == 16);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+  REQUIRE(collector.allocate(16) != nullptr);
+  REQUIRE(collector.get_stats().n_alive == 2);
+  REQUIRE(collector.get_stats().n_blocks == 3);
+  REQUIRE(collector.get_stats().bytes_allocated == 16 + 24);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+  REQUIRE(collector.allocate(24) != nullptr);
+  REQUIRE(collector.get_stats().n_alive == 3);
+  REQUIRE(collector.get_stats().n_blocks == 4);
+  REQUIRE(collector.get_stats().bytes_allocated == 16 + 24 + 32);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+  collector.collect();
+  REQUIRE(collector.get_stats().n_alive == 0);
+  REQUIRE(collector.get_stats().n_blocks == 4);
+  REQUIRE(collector.get_stats().bytes_allocated == 0);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+}
 
-    auto a_12 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
-    auto a_15 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
-    auto b_7 = reinterpret_cast<B *>(collector.allocate(sizeof(B)));
-    auto a_37 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
-    auto a_59 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
-    auto b_9 = reinterpret_cast<B *>(collector.allocate(sizeof(B)));
-    auto a_20 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
+TEST_CASE("collect - one alive object") {
+  gc::MarkAndSweep collector(1000);
+  void *obj = collector.allocate(8);
+  REQUIRE(obj != nullptr);
+  REQUIRE(collector.get_stats().n_alive == 1);
+  REQUIRE(collector.get_stats().n_blocks == 2);
+  REQUIRE(collector.get_stats().bytes_allocated == 16);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+  collector.push_root(&obj);
+  collector.collect();
+  REQUIRE(collector.get_stats().n_alive == 1);
+  REQUIRE(collector.get_stats().n_blocks == 2);
+  REQUIRE(collector.get_stats().bytes_allocated == 16);
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+}
 
-    a_15->x = a_12;
-    a_15->y = a_37;
-    a_37->x = a_20;
-    a_37->y = a_59;
+TEST_CASE("collect - example 13.4 (A. Appel)") {
+  gc::MarkAndSweep collector(1000);
 
-    b_7->z = b_9;
-    b_9->z = b_7;
+  auto a_12 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
+  auto a_15 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
+  auto b_7 = reinterpret_cast<B *>(collector.allocate(sizeof(B)));
+  auto a_37 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
+  auto a_59 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
+  auto b_9 = reinterpret_cast<B *>(collector.allocate(sizeof(B)));
+  auto a_20 = reinterpret_cast<A *>(collector.allocate(sizeof(A)));
 
-    REQUIRE(collector.get_stats().n_alive == 7);
-    REQUIRE(collector.get_stats().n_blocks == 8);
-    REQUIRE(collector.get_stats().bytes_allocated ==
-            (5 * (8 + 16) + 2 * (8 + 8)));
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
+  a_15->x = a_12;
+  a_15->y = a_37;
+  a_37->x = a_20;
+  a_37->y = a_59;
 
-    collector.push_root(reinterpret_cast<void **>(&a_15));
-    collector.push_root(reinterpret_cast<void **>(&a_37));
+  b_7->z = b_9;
+  b_9->z = b_7;
 
-    collector.collect();
-    REQUIRE(collector.get_stats().n_alive == 5);
-    REQUIRE(collector.get_stats().n_blocks == 8);
-    REQUIRE(collector.get_stats().bytes_allocated == 5 * (8 + 16));
-    REQUIRE(collector.get_stats().bytes_allocated +
-                collector.get_stats().bytes_free ==
-            1000);
-  }
+  REQUIRE(collector.get_stats().n_alive == 7);
+  REQUIRE(collector.get_stats().n_blocks == 8);
+  REQUIRE(collector.get_stats().bytes_allocated ==
+          (5 * (8 + 16) + 2 * (8 + 8)));
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
+
+  collector.push_root(reinterpret_cast<void **>(&a_15));
+  collector.push_root(reinterpret_cast<void **>(&a_37));
+
+  collector.collect();
+  REQUIRE(collector.get_stats().n_alive == 5);
+  REQUIRE(collector.get_stats().n_blocks == 8);
+  REQUIRE(collector.get_stats().bytes_allocated == 5 * (8 + 16));
+  REQUIRE(collector.get_stats().bytes_allocated +
+              collector.get_stats().bytes_free ==
+          1000);
 }
